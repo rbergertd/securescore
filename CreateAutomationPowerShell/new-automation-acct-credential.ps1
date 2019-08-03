cfee8fd53ef24467c5c3de6377aaa37c712eb288
Ryan Berger <ryan.berger@techdata.com>
2019-08-03 13:59:08 -0400

    Update new-automation-acct-credential.ps1

--- CreateAutomationPowerShell/new-automation-acct-credential.ps1
+++ CreateAutomationPowerShell/new-automation-acct-credential.ps1
@@ -1,9 +1,12 @@
 ﻿$ResourceGroup = 'RB-SecureScoreAA'
 $AutomationAccountName = 'SecureScore'
 $Location = 'EastUS2'
-$User = 'SecureScore@tdsolutionfactory.onmicrosoft.com'
-$StringPassWord ='C@Pc0m10'
-$Password = ConvertTo-SecureString $StringPassWord -AsPlainText -Force
+$StartTime = Get-Date "23:59:00"
+$EndTime = $StartTime.AddYears(5)
+$User = 'SecureScore@domain.onmicrosoft.com'
+$ScheduleName = 'SecureScore Daily Run'
+#$StringPassWord ='C@Pc0m10'
+$Password = ConvertTo-SecureString "M1chA3lbrie183" -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $Password
 
 # Manually connect to Azure - we need to automate both of these with a Vertex deployment
@@ -25,12 +28,16 @@ New-AzureRmResourceGroup -Name $ResourceGroup -Location $Location
 Start-Sleep 10
 New-AzureRmAutomationAccount -Name $AutomationAccountName -Location $Location -ResourceGroupName $ResourceGroup -Plan Free
 Start-Sleep 30
+New-AzureRmAutomationSchedule -AutomationAccountName $AutomationAccountName -Name $ScheduleName -StartTime $StartTime -ExpiryTime $EndTime -DayInterval 1 -ResourceGroupName $ResourceGroup
 New-AzureRmAutomationCredential -AutomationAccountName $AutomationAccountName -Name "MSOnline" -Value $Credential -ResourceGroupName $ResourceGroup
 Start-Sleep 10
 New-AzureRmAutomationModule -Name MSOnline -ContentLinkUri "https://github.com/rbergertd/securescore/raw/master/Modules/MSOnline.zip" -ResourceGroupName $ResourceGroup -AutomationAccountName $AutomationAccountName
 Start-Sleep 15
 New-AzureRmAutomationModule -Name AzureAD -ContentLinkUri "https://github.com/rbergertd/securescore/raw/master/Modules/AzureAD.zip" -ResourceGroupName $ResourceGroup -AutomationAccountName $AutomationAccountName
 Start-Sleep 15
-Import-AzureRmAutomationRunbook -ResourceGroup $ResourceGroup –AutomationAccountName $automationAccountName –Name TestRunBookPSImport -Type PowerShell –Path "C:\Scripts\test-msol-creds.ps1"
-
+# Importing RunBooks 
+# We'll run an import, publish the imported runbook, and then register the runbook to the schedule we created so that it runs daily at 24:00.
+Import-AzureRmAutomationRunbook -ResourceGroup $ResourceGroup –AutomationAccountName $AutomationAccountName –Name Enable-MailBoxAudit-AuditRecording -Type PowerShell –Path "C:\Scripts\Enable-MailBoxAudit-AuditRecording.ps1"
+Publish-AzureRmAutomationRunbook -AutomationAccountName $AutomationAccountName -Name Enable-MailBoxAudit-AuditRecording -ResourceGroupName $ResourceGroup
+Register-AzureRmAutomationScheduledRunbook -AutomationAccountName $AutomationAccountName -Name Enable-MailBoxAudit-AuditRecording -ScheduleName $ScheduleName -ResourceGroupName $ResourceGroup
 
