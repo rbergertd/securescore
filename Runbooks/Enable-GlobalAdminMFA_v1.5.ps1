67a5a56337c1be614add8cc768f281acb893b4f6
Ryan Berger <45945983+rbergertd@users.noreply.github.com>
2020-04-29 07:00:26 -0400

    Create Enable-GlobalAdminMFA_v1.5.ps1

---
+++ Runbooks/Enable-GlobalAdminMFA_v1.5.ps1
@@ -0,0 +1,32 @@
+$cred = Get-AutomationPSCredential -Name "MSOnline"
+Connect-AzureAD -Credential $cred
+Connect-MsolService -Credential $cred
+
+#Enable MFA for "Company Administrators" aka Global Admins
+
+$multiFactor = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+$multiFactor.RelyingParty = "*"
+$multiFactor.State = "Enforced"
+$multiFactor.RememberDevicesNotIssuedBefore = (Get-Date) 
+$multiFactorOff = @()
+$domains = Get-MsolDomain
+$secureScoreUser = "SecureScore@$($Domains[0].Name)"
+
+
+#For all users turn MFA on
+Get-MsolUser | ForEach-Object {
+     Set-MsolUser -UserPrincipalName $_.UserPrincipalName -StrongAuthenticationRequirements $multiFactor
+}
+
+#Turn off MFA for the SecureScore user. This is done explicitly in case SecureScore user is not in BreakGlass group yet
+Set-MsolUser -UserPrincipalName $secureScoreUser -StrongAuthenticationRequirements $multiFactorOff 
+
+#If there is a group called "SecureScoreBreakGlass" then exclude the additional users (i.e. Turn MFA Off for them)
+#Else create the SecureScoreBreakGlass group
+if ((Get-MsolGroup -SearchString "SecureScoreBreakGlass" | Measure-Object).Count -gt 0) {
+  Get-MsolGroupMember -GroupObjectId (Get-MsolGroup -SearchString "SecureScoreBreakGlass").ObjectId | ForEach-Object {
+    Set-MsolUser -UserPrincipalName $_.EmailAddress -StrongAuthenticationRequirements $multiFactorOff
+  }
+} else {
+  New-AzureADGroup -DisplayName "SecureScoreBreakGlass" -MailEnabled $false -SecurityEnabled $true -MailNickName "NotSet"
+}
